1eeaf05ccd1aa76a49cddc759d1aba26c61ff552
deluxscript <delsam94@yahoo.com>
2018-10-19 18:53:55 +0100

    stylings

--- resources/views/Public/ViewEvent/Layouts/EventPage.blade.php
+++ resources/views/Public/ViewEvent/Layouts/EventPage.blade.php
@@ -39,42 +39,172 @@
 
         <!--Bootstrap placeholder fix-->
         <style>
-            ::-webkit-input-placeholder { /* WebKit browsers */
-                color:    #ccc !important;
-            }
-            :-moz-placeholder { /* Mozilla Firefox 4 to 18 */
-                color:    #ccc !important;
-                opacity:  1;
-            }
-            ::-moz-placeholder { /* Mozilla Firefox 19+ */
-                color:    #ccc !important;
-                opacity:  1;
-            }
-            :-ms-input-placeholder { /* Internet Explorer 10+ */
-                color:    #ccc !important;
-            }
-
-            input, select {
-                color: #999 !important;
-            }
-
-            .btn {
-                color: #fff !important;
-            }
-
-            @font-face {
-                font-family: 'FuturaPTBook';
-                src: url('http://dev.tixy.ng/assets/stylesheet/icons/iconfont/fonts/FuturaPTBook.woff') format('woff'),
-                    url('http://dev.tixy.ng/assets/stylesheet/icons/iconfont/fonts/FuturaPTBook.ttf') format('truetype');
-                font-weight: normal;
-                font-style: normal;
-            }
-
-            .attendize, *  {
-                font-family: 'FuturaPTBook'!important;
-            }
-
-        </style>
+                ::-webkit-input-placeholder { /* WebKit browsers */
+                        color:    #ccc !important;
+                    }
+                    :-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+                        color:    #ccc !important;
+                        opacity:  1;
+                    }
+                    ::-moz-placeholder { /* Mozilla Firefox 19+ */
+                        color:    #ccc !important;
+                        opacity:  1;
+                    }
+                    :-ms-input-placeholder { /* Internet Explorer 10+ */
+                        color:    #ccc !important;
+                    }
+        
+                    input, select {
+                        color: #999 !important;
+                    }
+        
+                    .btn {
+                        color: #fff !important;
+                    }
+        
+                    @font-face {
+                        font-family: 'FuturaPTBook';
+                        src: url('http://dev.tixy.ng/assets/stylesheet/icons/iconfont/fonts/FuturaPTBook.woff') format('woff'),
+                            url('http://dev.tixy.ng/assets/stylesheet/icons/iconfont/fonts/FuturaPTBook.ttf') format('truetype');
+                        font-weight: normal;
+                        font-style: normal;
+                    }
+        
+                    .attendize, *  {
+                        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol" !important;
+                    }
+        
+                </style>
+        
+            <style>
+                body {
+                            background: url("./tixy-pattern.png");
+                            background-size: 1000px;
+                        }
+                    </style>
+        
+            <style>
+                body {
+                    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+                    -webkit-font-smoothing: antialiased;
+                    color: #495057;
+                }
+                #event_page_wrap {
+                    background: rgba(255,255,255,0.4)
+                }
+                .ticket {
+                    color: #495057;
+                }
+                .ticket-title {
+                    color: #212529;
+                }
+                .btn {
+                    text-transform: none;
+                    font-weight: 700;
+                    border-radius: 4px;
+                    transition: 250ms ease-in-out;
+                    text-shadow: none;
+                    border: none !important
+                }
+                .btn.btn-secondary {
+                    background: #dbe4ff;
+                    color: #1e2edb !important;
+                    margin-bottom: 16px
+                }
+                .btn.btn-primary {
+                    background-color: #1e2edb !important;
+                }
+                .btn:hover {
+                    opacity: 0.8
+                }
+                .btn.disabled, .btn[disabled] {
+                    background: #e9ecef;
+                    color: #868e96 !important
+                }
+                #event_page_wrap #intro .event_buttons .btn-event-link {
+                    text-transform: none;
+                    letter-spacing: 0;
+                    background:#dbe4ff;
+                    color:#1e2edb !important;
+                    box-shadow: 0 8px 16px white
+                }
+                #event_page_wrap #organiserHead {
+                    background: rgba(0, 0, 0, 0.1);
+                    color: #212529
+                }
+                #event_page_wrap #intro h1 {
+                    font-weight: 700
+                }
+                #event_page_wrap #intro {
+                    color: #212529;
+                    padding-top: 64px
+                }
+                .container {
+                    border-radius: 4px;
+                    overflow: hidden;
+                    padding-bottom: 0;
+                    box-shadow: 0 32px 64px -8px rgba(0, 0, 0, 0.1);
+                    position: relative;
+                    z-index: 1
+                }
+                #intro {
+                    box-shadow: none
+                }
+                .section_head {
+                    font-weight: 700;
+                    letter-spacing: 0;
+                    color: #212529
+                }
+                label {
+                    color: #868e96
+                }
+                input[type="text"] {
+                    border-radius: 4px;
+                    border: none;
+                    background: #e9ecef;
+                    color: #212529 !important
+                }
+                .panel {
+                    border-radius: 4px;
+                    border-color: #dee2e6;
+                    box-shadow: 0 16px 32px -4px #dee2e6;
+                    border-bottom-width: 1px;
+                }
+                .panel-heading {
+                    padding-top: 4px;
+                }
+                .panel-footer {
+                    border-bottom-right-radius: 4px;
+                    border-bottom-left-radius: 4px;
+                    color: #212529 !important;
+                    background: #FCF7E9 !important;
+                    border-top: none
+                }
+                .panel-primary .panel-heading {
+                    background: #FCF7E9 !important;
+                    border-top-right-radius: 4px;
+                    border-top-left-radius: 4px;
+                    border-left: 1px solid #dee2e6 !important;
+                    border-top: 1px solid #dee2e6 !important;
+                    border-right: 1px solid #dee2e6 !important;
+                    color: #212529 !important
+                }
+                .rrssb-text {
+                    text-transform: capitalize;
+                }
+                #footer {
+                    background: none;
+                }
+        
+                #footer .container {
+                    box-shadow: none
+                }
+        
+                #footer a,
+                #footer {
+                    color: #495057
+                }
+                </style>
         @if ($event->bg_type == 'color' || Input::get('bg_color_preview'))
             <style>body {background-color: {{(Input::get('bg_color_preview') ? '#'.Input::get('bg_color_preview') : $event->bg_color)}} !important; }</style>
         @endif
@@ -82,8 +212,10 @@
         @if (($event->bg_type == 'image' || $event->bg_type == 'custom_image' || Input::get('bg_img_preview')) && !Input::get('bg_color_preview'))
             <style>
                 body {
-                    background: url({{(Input::get('bg_img_preview') ? '/'.Input::get('bg_img_preview') :  asset(config('attendize.cdn_url_static_assets').'/'.$event->bg_image_path))}}) no-repeat center center fixed;
-                    background-size: cover;
+                    /* background: url({{(Input::get('bg_img_preview') ? '/'.Input::get('bg_img_preview') :  asset(config('attendize.cdn_url_static_assets').'/'.$event->bg_image_path))}}) no-repeat center center fixed; */
+                    /* background-size: cover; */
+                    background: url('http://tixy-event-page.surge.sh/tixy-pattern.png');
+                    background-size: 1000px;
                 }
             </style>
         @endif
--- resources/views/Public/ViewOrganiser/Layouts/OrganiserPage.blade.php
+++ resources/views/Public/ViewOrganiser/Layouts/OrganiserPage.blade.php
@@ -31,6 +31,242 @@
         <![endif]-->
 
        {!!HTML::style('assets/stylesheet/frontend.css')!!}
+
+       <style>
+            body {
+                background-color: #EEEEEE !important;
+            }
+    
+            section#intro {
+                background-color: #76a867 !important;
+                color: #FFFFFF !important;
+            }
+    
+            .event-list>li>time {
+                color: #FFFFFF;
+                background-color: #76a867;
+            }
+        </style>
+        <style>
+            body,
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6,
+            .h1,
+            .h2,
+            .h3,
+            .h4,
+            .h5,
+            .h6 {
+                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+            }
+    
+            .event-listing-heading {
+                color: #212529;
+            }
+    
+            body {
+                -webkit-font-smoothing: antialiased;
+                color: #495057;
+                background-color: #f8f9fa
+            }
+    
+            h1,
+            .title a {
+                font-weight: 700
+            }
+    
+            .ticket {
+                color: #495057;
+            }
+    
+            .event-list li .info {
+                padding-right: 0;
+            }
+    
+            .event-list li {
+                border-radius: 4px;
+            }
+    
+            .event-list li .info,
+            .event-list li {
+                border-top-right-radius: 4px;
+                border-bottom-right-radius: 4px;
+                overflow: hidden;
+            }
+    
+            section#intro {
+                background-color: #fff9db !important;
+                color: #212529 !important;
+                margin-top: 0
+            }
+    
+            #organiser_page_wrap {
+                padding-top: 20px;
+                min-height: 100vh;
+                position: relative;
+                background: url("./tixy-pattern.png");
+                background-size: 1000px
+            }
+    
+            .event-list li .info ul {
+                background: #dbe4ff;
+            }
+    
+            .event-list li .info ul li a {
+                color: #1e2edb;
+                font-weight: 500
+            }
+    
+            .event-list>li>.info>ul>li:hover {
+                background: #bac8ff;
+                font-weight: 500
+            }
+    
+            .ticket-title {
+                color: #212529;
+            }
+    
+            .event-list li time .day {
+                font-size: 40px;
+                font-weight: 300;
+            }
+    
+            .event-list li time .day,
+            .event-list li time .month {
+                opacity: 0.8
+            }
+    
+            .event-list li time {
+                display: flex;
+                flex-direction: column;
+                align-content: center;
+                justify-content: center;
+                border-top-left-radius: 4px;
+                border-bottom-left-radius: 4px;
+                background: #fff9db;
+                color: #212529
+            }
+    
+            .btn {
+                text-transform: none;
+                font-weight: 700;
+                border-radius: 4px;
+                transition: 250ms ease-in-out;
+                text-shadow: none;
+                border: none !important
+            }
+    
+            .btn.btn-secondary {
+                background: #dbe4ff;
+                color: #1e2edb !important;
+                margin-bottom: 16px
+            }
+    
+            .btn.btn-primary {
+                background-color: #1e2edb !important;
+            }
+    
+            .btn:hover {
+                opacity: 0.8
+            }
+    
+            .btn.disabled,
+            .btn[disabled] {
+                background: #e9ecef;
+                color: #868e96 !important
+            }
+    
+            #event_page_wrap #intro .event_buttons .btn-event-link {
+                text-transform: none;
+                letter-spacing: 0
+            }
+    
+            #event_page_wrap #intro h1 {
+                font-weight: 700
+            }
+    
+            .container {
+                border-radius: 4px;
+                overflow: hidden;
+                padding-bottom: 0;
+                box-shadow: 0 32px 64px -8px rgba(0, 0, 0, 0.1);
+                position: relative;
+                z-index: 1
+            }
+    
+            #intro {
+                border-bottom-right-radius: 0;
+                border-bottom-left-radius: 0;
+            }
+    
+            .section_head {
+                font-weight: 700;
+                letter-spacing: 0;
+                color: #212529
+            }
+    
+            label {
+                color: #868e96
+            }
+    
+            input[type="text"] {
+                border-radius: 4px;
+                border: none;
+                background: #e9ecef;
+                color: #212529 !important
+            }
+    
+            .panel {
+                border-radius: 4px;
+                border-color: #dee2e6;
+                box-shadow: 0 16px 32px -4px #dee2e6;
+                border-bottom-width: 1px;
+            }
+    
+            .panel-heading {
+                padding-top: 4px;
+            }
+    
+            .panel-footer {
+                border-bottom-right-radius: 4px;
+                border-bottom-left-radius: 4px;
+                color: #212529 !important;
+                background: #FCF7E9 !important;
+                border-top: none
+            }
+    
+            .panel-primary .panel-heading {
+                background: #FCF7E9 !important;
+                border-top-right-radius: 4px;
+                border-top-left-radius: 4px;
+                border-left: 1px solid #dee2e6 !important;
+                border-top: 1px solid #dee2e6 !important;
+                border-right: 1px solid #dee2e6 !important;
+                color: #212529 !important
+            }
+    
+            .rrssb-text {
+                text-transform: capitalize;
+            }
+    
+            #footer {
+                background: none;
+            }
+    
+            #footer .container {
+                box-shadow: none
+            }
+    
+            #footer a,
+            #footer {
+                color: #495057
+            }
+        </style>
+        
         @yield('head')
     </head>
     <body class="attendize">
