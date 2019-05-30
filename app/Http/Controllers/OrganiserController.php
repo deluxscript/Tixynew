5a30280be74cb04b7639b3210f1ea97c536a514d
deluxscript <delsam94@yahoo.com>
2019-05-30 11:55:44 +0200

    organiser

--- app/Http/Controllers/OrganiserController.php
+++ app/Http/Controllers/OrganiserController.php
@@ -18,13 +18,13 @@ class OrganiserController extends MyBaseController
      */
     public function showSelectOrganiser()
     {   
-        $organiser = Organiser::findOrFail($organiser_id);
+        $organiser = Organiser::findOrFail();
         $logged_in = Auth::user();
 
         if (!$organiser->enable_organiser_page && !Utils::userOwns($organiser)) {
             abort(404);
         }
-        
+
         $data = [
             'organiser'       => $organiser,
             'user'       => $logged_in
