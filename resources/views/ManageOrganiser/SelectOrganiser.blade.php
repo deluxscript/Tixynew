d385cc9b6a5a3345c0f71b232b2763df47b1a88b
deluxscript <delsam94@yahoo.com>
2019-05-30 12:04:28 +0200

    organiser

--- resources/views/ManageOrganiser/SelectOrganiser.blade.php
+++ resources/views/ManageOrganiser/SelectOrganiser.blade.php
@@ -34,7 +34,7 @@
                         @endforeach
                     </div>
                     @endif
-                <p>{{ $organiser  }}</p>
+                <p>{{ $organiser->name }}</p>
 
                     <div style="margin-top:-15px; padding: 10px; text-align: center;">
                         OR
