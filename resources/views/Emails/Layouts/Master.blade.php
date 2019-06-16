57e6ab72115874d09c95f9c7c2dbad7982508fd4
deluxscript <delsam94@yahoo.com>
2019-06-17 00:48:08 +0200

    email layout

--- resources/views/Emails/Layouts/Master.blade.php
+++ resources/views/Emails/Layouts/Master.blade.php
@@ -2544,6 +2544,19 @@ Margins, paddings
                     </td>
                   </tr>
                 </table>
+                <div class="main-content" style="box-sizing: border-box;">
+                    <img
+                      src="url({{asset('assets/images/default-banner.jpg')}})"
+                      class=" va-middle border-left border-right border-top"
+                      alt="thumbs up"
+                      style="border-top-right-radius:4px;border-top-left-radius:4px;margin-bottom:-2px;z-index:100;width: 100%;box-sizing: border-box;"
+                    />
+                    <table class="box" cellpadding="0" cellspacing="0">
+                      <tr>
+                        <td>
+                          <table cellpadding="0" cellspacing="0">
+                            <tr>
+                              <td class="content">
                 @yield('message_content')
 
 <!-- /body -->
