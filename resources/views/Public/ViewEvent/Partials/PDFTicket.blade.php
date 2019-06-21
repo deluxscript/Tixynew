6ddd575dfddcc05f41739b2c2c5e2690a1499e19
deluxscript <delsam94@yahoo.com>
2019-06-21 03:46:27 +0200

    new setup

--- resources/views/Public/ViewEvent/Partials/PDFTicket.blade.php
+++ resources/views/Public/ViewEvent/Partials/PDFTicket.blade.php
@@ -41,144 +41,7 @@
       </style>
     <![endif]-->
     
-  <style>body {
-margin: 0; padding: 0; background-color: #f5f7fb; font-size: 15px; line-height: 160%; mso-line-height-rule: exactly; color: #444444; width: 100%;
-}
-body {
-font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;
-}
-img {
-border: 0 none; line-height: 100%; outline: none; text-decoration: none; vertical-align: baseline; font-size: 0;
-}
-a:hover {
-text-decoration: underline;
-}
-.btn:hover {
-text-decoration: none;
-}
-.btn.bg-bordered:hover {
-background-color: #f9fbfe !important;
-}
-a.bg-blue:hover {
-background-color: #3a77cc !important;
-}
-a.bg-azure:hover {
-background-color: #37a3f1 !important;
-}
-a.bg-indigo:hover {
-background-color: #596ac9 !important;
-}
-a.bg-purple:hover {
-background-color: #9d50e8 !important;
-}
-a.bg-pink:hover {
-background-color: #f55f91 !important;
-}
-a.bg-red:hover {
-background-color: #c01e1d !important;
-}
-a.bg-orange:hover {
-background-color: #fd8e35 !important;
-}
-a.bg-yellow:hover {
-background-color: #e3b90d !important;
-}
-a.bg-lime:hover {
-background-color: #73cb2d !important;
-}
-a.bg-green:hover {
-background-color: #56ab00 !important;
-}
-a.bg-teal:hover {
-background-color: #28beae !important;
-}
-a.bg-cyan:hover {
-background-color: #1596aa !important;
-}
-a.bg-gray:hover {
-background-color: #95a9b0 !important;
-}
-a.bg-secondary:hover {
-background-color: #ecf0f2 !important;
-}
-.img-hover:hover img {
-opacity: .64;
-}
-@media only screen and (max-width: 560px) {
-  body {
-    font-size: 14px !important;
-  }
-  .content {
-    padding: 24px !important;
-  }
-  .content-image-text {
-    padding: 24px !important;
-  }
-  .content-image {
-    height: 100px !important;
-  }
-  .content-image-text {
-    padding-top: 96px !important;
-  }
-  h1 {
-    font-size: 24px !important;
-  }
-  .h1 {
-    font-size: 24px !important;
-  }
-  h2 {
-    font-size: 20px !important;
-  }
-  .h2 {
-    font-size: 20px !important;
-  }
-  h3 {
-    font-size: 18px !important;
-  }
-  .h3 {
-    font-size: 18px !important;
-  }
-  .col {
-    display: table !important; width: 100% !important;
-  }
-  .col-spacer {
-    display: table !important; width: 100% !important;
-  }
-  .col-spacer-xs {
-    display: table !important; width: 100% !important;
-  }
-  .col-spacer-sm {
-    display: table !important; width: 100% !important;
-  }
-  .col-hr {
-    display: table !important; width: 100% !important;
-  }
-  .row {
-    display: table !important; width: 100% !important;
-  }
-  .col-hr {
-    border: 0 !important; height: 24px !important; width: auto !important; background: transparent !important;
-  }
-  .col-spacer {
-    width: 100% !important; height: 24px !important;
-  }
-  .col-spacer-sm {
-    height: 16px !important;
-  }
-  .col-spacer-xs {
-    height: 8px !important;
-  }
-  .chart-cell-spacer {
-    width: 4px !important;
-  }
-  .text-mobile-center {
-    text-align: center !important;
-  }
-  .d-mobile-none {
-    display: none !important;
-  }
-}
-</style></head>
+  </head>
 
   <body class="bg-body" style="font-size: 15px; margin: 0; padding: 0; line-height: 160%; mso-line-height-rule: exactly; color: #444444; width: 100%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;" bgcolor="#f5f7fb">
     <center>
@@ -203,7 +66,7 @@ opacity: .64;
                           <table cellpadding="0" cellspacing="0" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; border-collapse: collapse; width: 100%;">
                             <tr>
                               <td class="content border-top" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; border-top-width: 1px; border-top-color: #f0f0f0; border-top-style: solid; padding: 40px 48px;">
-                                <a href="https://tixy.ng/" style="color: #467fcf; text-decoration: none;"><img src="{{asset('assets/images/default-banner.jpg')}}" height="32" alt="" style="line-height: 100%; outline: none; text-decoration: none; vertical-align: baseline; font-size: 0; border: 0 none;" /></a>
+                                <a href="https://tixy.ng/" style="color: #467fcf; text-decoration: none;"><img src="{{asset('assets/images/yellow-mark.png')}}" height="32" alt="" style="line-height: 100%; outline: none; text-decoration: none; vertical-align: baseline; font-size: 0; border: 0 none;" /></a>
                                 <br />
                                 <br />
                                 <div class="barcode">
@@ -222,62 +85,62 @@ opacity: .64;
                                     <td class="col" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 0;" valign="top"></td>
                                   </tr>
                                   <tr>
-                                    <td style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Event</td>
-                                    <td class="font-strong text-right" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
+                                    <td style="width: 30%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Event</td>
+                                    <td class="font-strong text-right" style="width: 70%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
                                       {{$event->title}}
                                     </td>
                                   </tr>
                                   <tr>
-                                    <td style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Name</td>
-                                    <td class="font-strong text-right" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
+                                    <td style="width: 30%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Name</td>
+                                    <td class="font-strong text-right" style="width: 70%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
                                       {{$attendee->first_name.' '.$attendee->last_name}}
                                     </td>
                                   </tr>
                                   <tr>
-                                    <td style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Organiser</td>
-                                    <td class="font-strong text-right" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
+                                    <td style="width: 30%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Organiser</td>
+                                    <td class="font-strong text-right" style="width: 70%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
                                      {{$event->organiser->name}}
                                     </td>
                                   </tr>
                                   <tr>
-                                    <td style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Ticket Type</td>
-                                    <td class="font-strong text-right" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
+                                    <td style="width: 30%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Ticket Type</td>
+                                    <td class="font-strong text-right" style="width: 70%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
                                       {{$attendee->ticket->title}}
                                     </td>
                                   </tr>
                                   <tr>
-                                    <td style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Venue</td>
-                                    <td class="font-strong text-right" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
+                                    <td style="width: 30%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Venue</td>
+                                    <td class="font-strong text-right" style="width: 70%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
                                       {{$event->venue_name}}
                                     </td>
                                   </tr>
                                   <tr>
-                                    <td style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Order Reference</td>
-                                    <td class="font-strong text-right" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
+                                    <td style="width: 30%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Order Reference</td>
+                                    <td class="font-strong text-right" style="width: 70%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
                                       {{$order->order_reference}}
                                     </td>
                                   </tr>
                                   <tr>
-                                    <td style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Attendee Reference</td>
-                                    <td class="font-strong text-right" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
+                                    <td style="width: 30%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Attendee Reference</td>
+                                    <td class="font-strong text-right" style="width: 70%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
                                       {{$attendee->reference}}
                                     </td>
                                   </tr>
                                   <tr>
-                                    <td style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Start Date</td>
-                                    <td class="font-strong text-right" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
+                                    <td style="width: 30%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Start Date</td>
+                                    <td class="font-strong text-right" style="width: 70%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
                                       {{$event->start_date->format('M dS g:iA')}}
                                     </td>
                                   </tr>
                                   <tr>
-                                    <td style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">End Date</td>
-                                    <td class="font-strong text-right" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
+                                    <td style="width: 30%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">End Date</td>
+                                    <td class="font-strong text-right" style="width: 70%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
                                       {{$event->end_date->format('M dS g:iA')}}
                                     </td>
                                   </tr>
                                   <tr>
-                                    <td style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Price</td>
-                                    <td class="font-strong text-right" style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
+                                    <td style="width: 30%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 4px 12px 4px 0;">Price</td>
+                                    <td class="font-strong text-right" style="width: 70%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: 600; padding: 4px 0 4px 12px;" align="right">
                                       {{money($attendee->ticket->total_price, $order->event->currency)}} (inc. {{money($attendee->ticket->total_booking_fee, $order->event->currency)}} Fees)
                                     </td>
                                   </tr>
